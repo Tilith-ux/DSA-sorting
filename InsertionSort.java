c45ee9e38fcb03fad03b7551d7274d5c78a51962
Tilith-ux <tilithvinsara02@gmail.com>
2024-11-18 16:00:11 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,52 +2,6 @@ import java.util.Scanner;
 
 public class InsertionSort {
 
-    // Method for ascending order insertion sort
-    public static void insertionSortAscending(int[] arr) {
-        int n = arr.length;
-
-        // Outer loop to iterate through the array
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];  // The element to be inserted
-            int j = i - 1;
-
-            // Move elements of arr[0..i-1] that are greater than key
-            // to one position ahead of their current position
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
-
-    // Method for descending order insertion sort
-    public static void insertionSortDescending(int[] arr) {
-        int n = arr.length;
-
-        // Outer loop to iterate through the array
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];  // The element to be inserted
-            int j = i - 1;
-
-            // Move elements of arr[0..i-1] that are smaller than key
-            // to one position ahead of their current position
-            while (j >= 0 && arr[j] < key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
-
-    // Utility function to print the array
-    public static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -66,17 +20,46 @@ public class InsertionSort {
 
         // Displaying the original array
         System.out.println("\nOriginal Array:");
-        printArray(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
         // Perform insertion sort in ascending order
-        insertionSortAscending(arr);
-        System.out.println("Sorted Array in Ascending Order:");
-        printArray(arr);
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];  // The element to be inserted
+            int j = i - 1;
 
-        // Perform insertion sort in descending order (same array)
-        insertionSortDescending(arr);
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+
+        // Display the sorted array in ascending order
+        System.out.println("Sorted Array in Ascending Order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Reverse the sorted array to get descending order
+        for (int i = 0; i < n / 2; i++) {
+            // Swap elements to reverse the array
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+
+        // Display the sorted array in descending order
         System.out.println("Sorted Array in Descending Order:");
-        printArray(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
         sc.close();
     }
